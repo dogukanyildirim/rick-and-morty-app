078a536253f932793bba623113d6e6705425631f
Dogukan Yildirim <dyildirim76@gmail.com>
2023-09-09 05:31:00 +0300

    main

---
+++ src/app/character/[characterId]/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+export default function CharacterDetails({ params: { characterId } }) {
+  const [character, setCharacter] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  function getSingleCharachters() {
+    console.log("istek atildi..");
+    fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setCharacter(resp);
+        setLoading(false);
+      })
+      .catch(console.error());
+  }
+
+  useEffect(() => {
+    getSingleCharachters();
+  }, []);
+
+  if (loading) {
+    return <div>loading...</div>
+  }
+  return (
+    <>
+      <div>
+        <h3>Karakter Detay Sayfası - character id..: {characterId}</h3>
+      </div>
+      <img src={character.image} />
+      <div>character name: {character.name}</div>
+      <div>character gender: {character.gender}</div>
+      <div>character origin: {character.origin.name}</div>
+    </>
+  );
+}
